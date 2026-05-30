e7263fb3cea0a22f70e4d3708c9e9d4453e4147d
aliomran5698 <wugfsfjhcdv12@gmail.com>
2026-05-30 17:42:15 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:newstask/Screens/SplashScreen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-import 'screens/SplashScreen.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: "https://pwbtejdshwvgownaqeao.supabase.co/rest/v1/L",
+    url: "https://pwbtejdshwvgownaqeao.supabase.co",
     anonKey: "sb_publishable_tR8Up4GHxG1mWG2Vh_pEYQ_nHCPYUqo",
   );
 
@@ -22,9 +20,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      theme: ThemeData(colorSchemeSeed: Colors.green, useMaterial3: true),
+      home: const SplashScreen(),
     );
   }
 }
