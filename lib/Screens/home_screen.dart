0a3780c8c9c8aa657bea26443bf4400f90952fc2
aliomran5698 <wugfsfjhcdv12@gmail.com>
2026-05-30 17:29:02 +0300

    Update home_screen.dart

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -14,7 +14,11 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List news = [];
 
+  List filteredNews = [];
+
   bool loading = true;
+
+  TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
@@ -28,7 +32,20 @@ class _HomeScreenState extends State<HomeScreen> {
 
     setState(() {
       news = data;
+
+      filteredNews = data;
+
       loading = false;
+    });
+  }
+
+  void searchNews(String value) {
+    setState(() {
+      filteredNews = news.where((item) {
+        final title = item['title'].toString().toLowerCase();
+
+        return title.contains(value.toLowerCase());
+      }).toList();
     });
   }
 
@@ -44,47 +61,158 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("News App"),
+      backgroundColor: const Color(0xffF4F4F4),
 
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+
+        title: const Text(
+          "All News",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+          ),
+        ),
         actions: [
-          IconButton(onPressed: logout, icon: const Icon(Icons.logout)),
+          IconButton(
+            onPressed: logout,
+            icon: const Icon(Icons.logout, color: Colors.black),
+          ),
         ],
       ),
 
       body: loading
           ? const Center(child: CircularProgressIndicator())
-          : news.isEmpty
-          ? const Center(child: Text("No News"))
-          : RefreshIndicator(
-              onRefresh: getNews,
+          : Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15),
 
-              child: ListView.builder(
-                itemCount: news.length,
+                  child: TextField(
+                    controller: searchController,
+                    onChanged: searchNews,
 
-                itemBuilder: (context, index) {
-                  final item = news[index];
+                    decoration: InputDecoration(
+                      hintText: "Search news...",
+                      prefixIcon: const Icon(Icons.search),
 
-                  return Card(
-                    margin: const EdgeInsets.all(10),
+                      filled: true,
+                      fillColor: Colors.white,
 
-                    child: ListTile(
-                      title: Text(item['title']),
-
-                      subtitle: Text(item['description'], maxLines: 2),
-
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) => DetailsScreen(news: item),
-                          ),
-                        );
-                      },
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide.none,
+                      ),
                     ),
-                  );
-                },
-              ),
+                  ),
+                ),
+
+                Expanded(
+                  child: RefreshIndicator(
+                    onRefresh: getNews,
+
+                    child: filteredNews.isEmpty
+                        ? const Center(child: Text("No News"))
+                        : ListView.builder(
+                            itemCount: filteredNews.length,
+
+                            itemBuilder: (context, index) {
+                              final item = filteredNews[index];
+
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (_) => DetailsScreen(news: item),
+                                    ),
+                                  );
+                                },
+
+                                child: Container(
+                                  margin: const EdgeInsets.symmetric(
+                                    horizontal: 15,
+                                    vertical: 10,
+                                  ),
+
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+
+                                    children: [
+                                      ClipRRect(
+                                        borderRadius:
+                                            const BorderRadius.vertical(
+                                              top: Radius.circular(30),
+                                            ),
+
+                                        child: Image.network(
+                                          item['image'],
+                                          height: 220,
+                                          width: double.infinity,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+
+                                      Padding(
+                                        padding: const EdgeInsets.all(20),
+
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+
+                                          children: [
+                                            const Text(
+                                              "4 MIN READ",
+
+                                              style: TextStyle(
+                                                color: Colors.grey,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+
+                                            const SizedBox(height: 15),
+
+                                            Text(
+                                              item['title'],
+
+                                              style: const TextStyle(
+                                                fontSize: 28,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+
+                                            const SizedBox(height: 15),
+
+                                            Text(
+                                              item['description'],
+                                              maxLines: 2,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: const TextStyle(
+                                                fontSize: 18,
+                                                color: Colors.black54,
+                                                height: 1.5,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                  ),
+                ),
+              ],
             ),
     );
   }
